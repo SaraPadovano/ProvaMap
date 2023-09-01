7706ec7701ec5f224aa40038aa0b7c1c63f93458
SaraPadovano <padovano.sara23@gmail.com>
2023-09-01 10:14:05 +0200

    fatto l'upload dell'immagine iniziale dell'avventura

--- adventure/src/main/java/di/uniba/map/b/adventure/swing/Start.java
+++ adventure/src/main/java/di/uniba/map/b/adventure/swing/Start.java
@@ -5,6 +5,8 @@
 package di.uniba.map.b.adventure.swing;
 
 import java.awt.Color;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -32,8 +34,8 @@ public class Start extends JFrame {
        fr = new JFrame();
         fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         panel = new JPanel();
-        
-            image=new ImageIcon(getClass().getResource("forest.jpg"));
+       
+            image=new ImageIcon("./resources/forest.jpg");
             displayField = new JLabel(image);
             
         panel.add(displayField);
